a240d6fba4c437174d174bfc69a75bbee1c0b2ba
Adeoye Damilare <damilare203@gmail.com>
2023-10-06 14:46:39 +0100

    second update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const headerContainer = document.querySelector('.header_container');
+const navbarEl = document.querySelector('.navbar')
 
 window.onscroll = () => {
     if (window.scrollY > 100) {
         headerContainer.classList.add('nav-active');
+        navbarEl.classList.add('nav-active');
     } else {
         headerContainer.classList.remove('nav-active');
+        navbarEl.classList.remove('nav-active');
+
     }
 }
